e9df56101f537390b0e3e153ae1867e1eaee5beb
tusharXsuvera <developer1.suvera@gmail.com>
2025-01-22 16:55:15 +0530

    # works page UI and home UI partially done

--- src/components/CustomTitle.tsx
+++ src/components/CustomTitle.tsx
@@ -3,10 +3,10 @@ import { CustomTitleProps } from "../interfaces/common";
 function CustomTitle(props: CustomTitleProps) {
   const { heading, title, subtitle } = props.items;
   return (
-    <div className="flex-col gap-5 items-center">
-      <span>{heading}</span>
-      <h1>{title}</h1>
-      <span>{subtitle}</span>
+    <div className="flex flex-col gap-2 items-center">
+      <span className="t4 uppercase font-mono tracking-widest">{heading}</span>
+      <h1 className="font-serif">{title}</h1>
+      <span className="t4">{subtitle}</span>
     </div>
   );
 }
